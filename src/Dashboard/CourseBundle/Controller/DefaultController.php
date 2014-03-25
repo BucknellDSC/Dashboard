1e02ca7e5d68a01d4eac1b3a66f7ca64b0fbb47e
lr030 <lr030@Bucknell.edu>
2014-03-25 13:44:04 -0400

    Any new
    ?

--- src/Dashboard/CourseBundle/Controller/DefaultController.php
+++ src/Dashboard/CourseBundle/Controller/DefaultController.php
@@ -5,15 +5,28 @@ namespace Dashboard\CourseBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Dashboard\CourseBundle\Entity\Course;
+use Symfony\Component\httpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
-    }
+	
+	public function newAction(Request $request)
+	{
+		// create a course and give it some dummy data for this example
+		$assignment = new Assignment();
+		$assignment->setName('TestAssignment');
+		
+		$assignment->setBriefDescr('This is a test assignment');
+		$form = $this->createFormBuilder($assignment)
+			->add('assignment', 'string')
+			->add('BriefDescr', 'text')
+			->add('save', 'submit')
+			->getForm();
+			
+		return $this->render('DashboardTaskBundle:Default:index.html.twig', array(
+		'form' => $form->createView(),)
+		);
+		
+		}
 }
