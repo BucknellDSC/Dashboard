b936ef91924e27b9be88a99982c08509d0cc5cad
lr030 <lr030@Bucknell.edu>
2014-04-09 14:32:59 -0400

    Create a form type-AssignmentType

---
+++ src/Dashboard/AssignmentBundle/Form/Type/AssignmentType.php
@@ -0,0 +1,33 @@
+<?php
+namespace Dashboard\AssignmentBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class AssignmentType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			//->add('assignment')
+			->add('name', 'text')
+			->add('BriefDescr', 'text')
+            ->add('LongDescr', 'text')			
+			->add('Course', 'entity', array(
+				'class' => 'DashboardAssignmentBundle:Course',
+				'property' => 'name',
+                                'multiple' => true,
+			))
+                        	->add('StudentsEnrolled', 'integer')
+                                ->add('TechCatory', 'text')
+                                ->add('TechTools', 'text')
+                                ->add('Showcase', 'checkbox')
+                                ->add('ProjectURL', 'text')
+			->add('save', 'submit');
+	}	
+	
+	public function getName()
+	{
+		return 'assignment';	
+	}
+}
